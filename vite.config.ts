f062392cac5ed42230c78b00d477586424f79438
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2022-06-02 19:22:58 +0900

    build: add vanilla-extract deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import { defineConfig } from "vitest/config"
 import react from '@vitejs/plugin-react'
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin()
+  ],
   resolve: {
     alias: {
       "@": path.resolve( __dirname, "src")
