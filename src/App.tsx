bd24ec83790626813fb459132d251058cf71ddbf
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2022-06-05 10:25:10 +0900

    feat: add title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { Helmet } from "react-helmet"
 import { Prant } from '@/features/prants/components'
 import { useSystem } from '@/features/system/hooks'
 import { appStyle } from "./App.css"
 import { Switcher } from '@/components/Switcher'
 import { StatusInfo } from './features/system/components'
+import seedImg from "@/src/assets/images/seed.png"
+
 
 function App() { 
   const { prants, seeds, increaseByClass, increasableByClass } = useSystem()
 
   return (
     <div className={ appStyle }>
+      <Helmet>
+        <title>Prant Clicker</title>
+      </Helmet>
       <h1>PrantClicker</h1>
       <StatusInfo seeds={ seeds }/>
       <Switcher>
